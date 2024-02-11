5073bb80b5392ad706c59d4ffd096102b85a4de3
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-02-12 02:46:46 +0530

    add some user authentication code

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req ,res) {
-    res.render("index");
+    res.render("index", { isAuthenticated: req.isAuthenticated() });
     
 })
 
